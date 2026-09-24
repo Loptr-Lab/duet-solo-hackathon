ae7d5162d9c19a66b63ff923e839be0df06dbc90
Dominique Devereaux <90852374+ibloud@users.noreply.github.com>
2026-09-24 04:40:02 -0500

    refactor: expose M1 client boundary to web client

---
+++ public/client/ITransport.js
@@ -0,0 +1,8 @@
+window.DuetITransport = class DuetITransport {
+  async connect() { throw new Error('ITransport.connect() must be implemented'); }
+  disconnect() { throw new Error('ITransport.disconnect() must be implemented'); }
+  request(_message, _payload) { throw new Error('ITransport.request() must be implemented'); }
+  on(_message, _handler) { throw new Error('ITransport.on() must be implemented'); }
+  off(_message, _handler) { throw new Error('ITransport.off() must be implemented'); }
+  isConnected() { throw new Error('ITransport.isConnected() must be implemented'); }
+};
